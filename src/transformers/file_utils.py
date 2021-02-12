34e764f4a3fbbe78234b62262d21239365646dcf
Piyush Ghai <ghai.8@osu.edu>
2021-02-12 04:57:07 -0800

    EC2 hack

--- src/transformers/file_utils.py
+++ src/transformers/file_utils.py
@@ -310,17 +310,18 @@ def is_pandas_available():
 
 
 def is_sagemaker_distributed_available():
+    return True  # Hack to get it working on EC2
     # Get the sagemaker specific env variable.
-    sagemaker_params = os.getenv("SM_FRAMEWORK_PARAMS", "{}")
-    try:
-        # Parse it and check the field "sagemaker_distributed_dataparallel_enabled".
-        sagemaker_params = json.loads(sagemaker_params)
-        if not sagemaker_params.get("sagemaker_distributed_dataparallel_enabled", False):
-            return False
-    except json.JSONDecodeError:
-        return False
-    # Lastly, check if the `smdistributed` module is present.
-    return importlib.util.find_spec("smdistributed") is not None
+    # sagemaker_params = os.getenv("SM_FRAMEWORK_PARAMS", "{}")
+    # try:
+    #     # Parse it and check the field "sagemaker_distributed_dataparallel_enabled".
+    #     sagemaker_params = json.loads(sagemaker_params)
+    #     if not sagemaker_params.get("sagemaker_distributed_dataparallel_enabled", False):
+    #         return False
+    # except json.JSONDecodeError:
+    #     return False
+    # # Lastly, check if the `smdistributed` module is present.
+    # return importlib.util.find_spec("smdistributed") is not None
 
 
 def is_soundfile_availble():
